5ba1fabdb8123eac910c13ea9bbf672505480067
ia0 <git@ia0.eu>
2016-01-17 19:15:58 +0100

    benches/diff.rs: Rename functions

--- benches/diff.rs
+++ benches/diff.rs
@@ -8,7 +8,7 @@ use test::Bencher;
 use rustc_serialize::base64::{FromBase64, ToBase64, STANDARD};
 use data_encoding::base64;
 
-fn encode<F: FnMut(&[u8]) -> Vec<u8>>(b: &mut Bencher, mut f: F) {
+fn encode<F: FnMut(&[u8]) -> String>(b: &mut Bencher, mut f: F) {
     let input = &[0u8; 4096];
     b.iter(|| f(input));
 }
@@ -19,21 +19,21 @@ fn decode<E, F: FnMut(&[u8]) -> Result<Vec<u8>, E>>(b: &mut Bencher, mut f: F) {
 }
 
 #[bench]
-fn data_encode(b: &mut Bencher) {
-    encode(b, |x| base64::encode(x).into_bytes());
+fn encode_crate(b: &mut Bencher) {
+    encode(b, base64::encode);
 }
 
 #[bench]
-fn rustc_encode(b: &mut Bencher) {
-    encode(b, |x| x.to_base64(STANDARD).into_bytes());
+fn encode_rustc(b: &mut Bencher) {
+    encode(b, |x| x.to_base64(STANDARD));
 }
 
 #[bench]
-fn data_decode(b: &mut Bencher) {
+fn decode_crate(b: &mut Bencher) {
     decode(b, base64::decode);
 }
 
 #[bench]
-fn rustc_decode(b: &mut Bencher) {
+fn decode_rustc(b: &mut Bencher) {
     decode(b, |x| x.from_base64());
 }
