ba3639050a14c2e5060e4a054ddcef4cb16bd4a2
Julien Cretin <cretin@google.com>
2017-04-17 18:18:27 +0200

    Test coverage at repo root

--- travis.sh
+++ travis.sh
@@ -31,29 +31,28 @@ git clean -fxd
 
 git clean -fxd
 
-( cd lib
-  info "Download kcov"
-  wget -q https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
+info "Download kcov"
+wget -q https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
 
-  info "Build kcov"
-  tar xf master.tar.gz
-  mkdir kcov-master/build
-  ( cd kcov-master/build
-    cmake ..
-    make >/dev/null
-  )
-
-  info "Test library with coverage"
-  cargo test --verbose --no-run
-  find ../target/debug -maxdepth 1 -type f -perm /u+x -printf '%P\n' |
-    while IFS= read -r test; do
-      info "Run $test"
-      ./kcov-master/build/src/kcov --verify --include-path=src/ \
-          ../target/kcov-"$test" ../target/debug/"$test"
-    done; unset test
-
-  info "Send coverage to coveralls.io"
-  ./kcov-master/build/src/kcov --coveralls-id="$TRAVIS_JOB_ID" \
-      --merge ../target/kcov ../target/kcov-*
+info "Build kcov"
+tar xf master.tar.gz
+mkdir kcov-master/build
+( cd kcov-master/build
+  cmake ..
+  make >/dev/null
 )
+
+info "Test library coverage"
+( cd lib; cargo test --verbose --no-run )
+find target/debug -maxdepth 1 -type f -perm /u+x -printf '%P\n' |
+  while IFS= read -r test; do
+    info "Run $test"
+    ./kcov-master/build/src/kcov --verify --include-path=lib/src/ \
+                                 target/kcov-"$test" ./target/debug/"$test"
+  done; unset test
+
+info "Send coverage to coveralls.io"
+./kcov-master/build/src/kcov --coveralls-id="$TRAVIS_JOB_ID" \
+                             --merge target/kcov target/kcov-*
+
 info "Done"
