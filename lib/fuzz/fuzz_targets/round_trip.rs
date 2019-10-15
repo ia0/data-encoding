9171cc1479b0761fb05f3cb31605b6853259c6b4
Julien Cretin <cretin@google.com>
2019-10-15 22:48:08 +0200

    Fuzz specification round trip

--- lib/fuzz/fuzz_targets/round_trip.rs
+++ lib/fuzz/fuzz_targets/round_trip.rs
@@ -9,6 +9,7 @@ use data_encoding_fuzz::{decode_prefix, generate_encoding};
 fuzz_target!(|data: &[u8]| {
     let mut data = data;
     let e = generate_encoding(&mut data);
+    assert_eq!(e.specification().encoding().unwrap(), e);
     assert_eq!(e.decode(e.encode(data).as_bytes()).unwrap(), data);
     if e.is_canonical() {
         let raw = decode_prefix(&e, &mut data);
