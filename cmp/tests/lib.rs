289dca9030b1f1db9abb625a84a409d6717f9521
Julien Cretin <cretin@google.com>
2020-11-01 01:50:04 +0100

    Update documentation

--- cmp/tests/lib.rs
+++ cmp/tests/lib.rs
@@ -1,13 +1,6 @@
-extern crate base64;
-extern crate cmp;
-extern crate data_encoding;
-#[macro_use]
-extern crate lazy_static;
-extern crate rustc_serialize;
-
+use base64::DecodeError::*;
 use data_encoding::DecodeKind::*;
-use data_encoding::{DecodeError, Encoding, Specification, BASE64};
-use rustc_serialize::base64::{FromBase64, ToBase64, STANDARD};
+use data_encoding::{DecodeError, BASE64};
 
 #[test]
 fn encode_exact() {
@@ -31,56 +24,29 @@ fn encode_exact() {
         BASE64.encode_mut(i, &mut r);
         assert_eq!(&r, o);
     }
-    for &(ref i, ref o) in tests {
-        assert_eq!(&i.to_base64(STANDARD).as_bytes(), o);
-    }
 }
 
 #[test]
 fn difference() {
     let x = b"AAB=";
     assert_eq!(BASE64.decode(x).err().unwrap(), DecodeError { position: 2, kind: Trailing });
-    assert_eq!(x.from_base64().unwrap(), vec![0, 0]);
-    assert!(base64::decode(x).is_err());
+    assert_eq!(base64::decode(x).err().unwrap(), InvalidLastSymbol(2, b'B'));
     let x = b"AA\nB=";
     assert_eq!(BASE64.decode(x).err().unwrap(), DecodeError { position: 4, kind: Length });
-    assert_eq!(x.from_base64().unwrap(), vec![0, 0]);
-    assert_eq!(base64::decode(x).err().unwrap(), base64::DecodeError::InvalidLength);
+    assert_eq!(base64::decode(x).err().unwrap(), InvalidLength);
     let x = b"AAB";
     assert_eq!(BASE64.decode(x).err().unwrap(), DecodeError { position: 0, kind: Length });
-    assert_eq!(x.from_base64().unwrap(), vec![0, 0]);
-    assert!(base64::decode(x).is_err());
+    assert_eq!(base64::decode(x).err().unwrap(), InvalidLastSymbol(2, b'B'));
     let x = b"AAA";
     assert_eq!(BASE64.decode(x).err().unwrap(), DecodeError { position: 0, kind: Length });
-    assert_eq!(x.from_base64().unwrap(), vec![0, 0]);
     assert_eq!(base64::decode(x).unwrap(), vec![0, 0]);
     let x = b"A\rA\nB=";
     assert_eq!(BASE64.decode(x).err().unwrap(), DecodeError { position: 4, kind: Length });
-    assert_eq!(x.from_base64().unwrap(), vec![0, 0]);
-    assert_eq!(base64::decode(x).err().unwrap(), base64::DecodeError::InvalidByte(1, b'\r'));
+    assert_eq!(base64::decode(x).err().unwrap(), InvalidByte(1, b'\r'));
     let x = b"-_\r\n";
     assert_eq!(BASE64.decode(x).err().unwrap(), DecodeError { position: 0, kind: Symbol });
-    assert_eq!(x.from_base64().unwrap(), vec![251]);
-    assert_eq!(base64::decode(x).err().unwrap(), base64::DecodeError::InvalidByte(0, b'-'));
+    assert_eq!(base64::decode(x).err().unwrap(), InvalidByte(0, b'-'));
     let x = b"AA==AA==";
     assert_eq!(BASE64.decode(x).unwrap(), vec![0, 0]);
-    assert!(x.from_base64().is_err());
-    assert_eq!(base64::decode(x).err().unwrap(), base64::DecodeError::InvalidByte(2, b'='));
-}
-
-lazy_static! {
-    static ref HEX: Encoding = {
-        let mut spec = Specification::new();
-        spec.symbols.push_str("0123456789abcdef");
-        spec.translate.from.push_str("ABCDEF");
-        spec.translate.to.push_str("abcdef");
-        spec.encoding().unwrap()
-    };
-}
-
-#[test]
-fn lazy_static_hex() {
-    assert_eq!(HEX.encode(b"Hello"), "48656c6c6f");
-    assert_eq!(HEX.decode(b"48656c6c6f").unwrap(), b"Hello");
-    assert_eq!(*HEX, data_encoding::HEXLOWER_PERMISSIVE);
+    assert_eq!(base64::decode(x).err().unwrap(), InvalidByte(2, b'='));
 }
--- lib/macro/README.md
+++ lib/macro/README.md
@@ -3,39 +3,7 @@ encoded strings (using common bases like base64, base32, or hexadecimal, and
 also custom bases). It also provides a macro to define compile-time custom
 encodings to be used with the [data-encoding] crate.
 
-If you were familiar with the [binary_macros] crate, this library is actually
-[inspired][binary_macros_issue] from it.
-
-### Examples
-
-You can define a compile-time byte slice or array (using the `hexlower` or
-`base64` macros for example):
-
-```rust
-const HELLO: &'static [u8] = &hexlower!("68656c6c6f");
-const FOOBAR: &'static [u8] = &base64!("Zm9vYmFy");
-// It is possible to define an array instead of a slice:
-hexlower_array!("const HELLO" = "68656c6c6f");
-base64_array!("const FOOBAR" = "Zm9vYmFy");
-```
-
-You can define a compile-time custom encoding using the `new_encoding` macro:
-
-```rust
-const HEX: Encoding = new_encoding!{
-    symbols: "0123456789abcdef",
-    translate_from: "ABCDEF",
-    translate_to: "abcdef",
-};
-const BASE64: Encoding = new_encoding!{
-    symbols: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
-    padding: '=',
-};
-```
-
 See the [documentation] for more details.
 
-[binary_macros]: https://crates.io/crates/binary_macros
-[binary_macros_issue]: https://github.com/ia0/data-encoding/issues/7
 [data-encoding]: https://crates.io/crates/data-encoding
 [documentation]: https://docs.rs/data-encoding-macro
