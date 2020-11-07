f4ebf25bb0aa9ea6a0f4ea84bef9fe021a46a43e
Julien Cretin <cretin@google.com>
2020-11-07 20:06:22 +0100

    Update travis

--- travis.sh
+++ travis.sh
@@ -2,111 +2,63 @@
 set -e
 
 info() {
-  echo -n "[1;36mInfo:[m $1"
-  if [ -n "$TOOLCHAIN" ]; then
-    echo -n " ($TOOLCHAIN)"
-  fi
-  echo
+  echo "[1;36m$1[m"
+}
+
+info_exec() {
+  local dir="$1"; shift
+  info "( cd $dir && $* )"
+  ( cd "${dir}" && "$@" )
 }
 
 test_lib() {
-  ( cd lib
-    [ -n "$1" ] && TOOLCHAIN="${1#+}"
-
-    info "Build library"
-    cargo $1 build --verbose
-
-    info "Test library"
-    cargo $1 test --verbose
-
-    info "Build noalloc library"
-    cargo $1 build --verbose --no-default-features
-
-    info "Build nostd library"
-    cargo $1 build --verbose --no-default-features --features=alloc
-
-    ( cd macro
-      info "Build macro library"
-      cargo $1 build --verbose
-
-      info "Test macro library"
-      cargo $1 test --verbose
-    )
-  )
+  info_exec lib cargo $1 build --verbose
+  info_exec lib cargo $1 test --verbose
+  info_exec lib cargo $1 build --verbose --no-default-features
+  info_exec lib cargo $1 build --verbose --no-default-features --features=alloc
+  info_exec lib/macro cargo $1 build --verbose
+  info_exec lib/macro cargo $1 test --verbose
 }
 
 bench_lib() {
-  ( cd lib
-
-    info "Benchmark library"
-    cargo bench --verbose
-  )
+  info_exec cargo lib bench --verbose
 }
 
 test_nostd() {
-  ( cd nostd
-    [ -n "$1" ] && TOOLCHAIN="${1#+}"
-
-    info "Test noalloc binary"
-    cargo $1 run --verbose --release
-
-    info "Test nostd binary"
-    cargo $1 run --verbose --release --features=alloc
-  )
+  info_exec nostd cargo $1 run --verbose --release
+  info_exec nostd cargo $1 run --verbose --release --features=alloc
 }
 
 test_bin() {
-  ( cd bin
-    [ -n "$1" ] && TOOLCHAIN="${1#+}"
-
-    info "Build binary"
-    cargo $1 build --verbose
-
-    info "Test binary"
-    cargo $1 test --verbose
-    ./test.sh $1
-  )
+  info_exec bin cargo $1 build --verbose
+  info_exec bin cargo $1 test --verbose
+  info_exec bin ./test.sh $1
 }
 
 bench_bin() {
-  ( cd bin
-
-    info "Benchmark binary"
-    ./bench.sh
-  )
+  info_exec bin ./bench.sh
 }
 
 test_outdated() {
-  ( info "Ensure cargo-outdated is installed"
-    which cargo-outdated >/dev/null || cargo install cargo-outdated
-
-    # Workaround error: failed to parse lock file at: data-encoding/Cargo.lock
-    # Caused by: invalid serialized PackageId for key `package.dependencies`
-    git clean -fxd
-
-    info "Test dependencies"
-    cargo outdated -w -R --exit-code=1
-  )
+  which cargo-outdated >/dev/null || info_exec cargo install cargo-outdated
+  # Workaround error: failed to parse lock file at: data-encoding/Cargo.lock
+  # Caused by: invalid serialized PackageId for key `package.dependencies`
+  info_exec git clean -fxd
+  info_exec cargo outdated -w -R --exit-code=1
 }
 
 send_coverage() {
-  ( git clean -fxd
-
-    info "Install tarpaulin"
-    which cargo-tarpaulin >/dev/null || cargo install cargo-tarpaulin
-
-    info "Test and send library coverage to coveralls.io"
-    ( cd lib
-      # We have to give an explicit list of --exclude-files due to
-      # https://github.com/xd009642/tarpaulin/issues/394
-      cargo tarpaulin --ciserver travis-ci --coveralls "$TRAVIS_JOB_ID" \
-        --exclude-files '../*' --exclude-files fuzz --exclude-files tests
-    )
-  ) || true
+  which cargo-tarpaulin >/dev/null || info_exec cargo install cargo-tarpaulin
+  info_exec git clean -fxd
+  # We have to give an explicit list of --exclude-files due to
+  # https://github.com/xd009642/tarpaulin/issues/394
+  info_exec lib cargo tarpaulin \
+    --ciserver=travis-ci --coveralls="$TRAVIS_JOB_ID" \
+    --exclude-files='../*' --exclude-files=fuzz --exclude-files=tests
 }
 
 if [ -n "$TRAVIS_JOB_ID" ]; then
-  git clean -fxd
+  info_exec git clean -fxd
   test_lib
   if [ "$TRAVIS_RUST_VERSION" = nightly ]; then
     bench_lib
@@ -116,7 +68,7 @@ if [ -n "$TRAVIS_JOB_ID" ]; then
   bench_bin
   test_outdated
   if [ "$TRAVIS_RUST_VERSION" = stable ]; then
-    send_coverage
+    send_coverage || true
   fi
 else
   test_lib +stable
